f7e617d3879d6f6b098f5108785d650b9fbff50f
Andrea Osmar <andrea88@live.se>
2021-06-06 22:01:35 +0200

    created details for thumbnails

--- hulu/components/Thumbnail.js
+++ hulu/components/Thumbnail.js
@@ -1,10 +1,14 @@
 import Image from 'next/image';
+import { ThumbUpIcon } from '@heroicons/react/outline';
+import { forwardRef } from 'react';
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
 	const BASE_URL = 'https://image.tmdb.org/t/p/original';
-	console.log(result);
+
 	return (
-		<div>
+		<div
+			ref={ref}
+			className='group cursor-pointer p-2 transition duration-200 ease-in-out transform sm:hover:scale-105 hover:z-50'>
 			<Image
 				layout='responsive'
 				src={
@@ -14,8 +18,20 @@ function Thumbnail({ result }) {
 				height={1080}
 				width={1920}
 			/>
+			<div className='p-2'>
+				<h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>
+					{result.title || result.original_name}
+				</h2>
+				<p className='truncate max-w-md'>{result.overview}</p>
+				<p className='flex items-center opacity-0 group-hover:opacity-100'>
+					{result.media_type && `${result.media_type} ∘`}{' '}
+					{result.release_date || result.first_air_date} ∘{' '}
+					<ThumbUpIcon className='h-5 mx-2' />
+					{result.vote_average}
+				</p>
+			</div>
 		</div>
 	);
-}
+});
 
 export default Thumbnail;
